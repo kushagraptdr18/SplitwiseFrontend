3cd87d0bcbbfcb071ada051b70b7516c0ff7861f
Kushagra Patidar <kushagraptdr18@gmail.com>
2025-04-08 15:27:57 +0530

    changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import AxiosInstance from "./utils/AxiosInstance";
+import AxiosInstance from "./utils/axios";
 
 function Login() {
   const [email, setEmail] = useState("");
