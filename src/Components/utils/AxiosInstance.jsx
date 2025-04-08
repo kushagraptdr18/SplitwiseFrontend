2fc07c645b27bd77106150a507a07fb23e6d783f
Kushagra Patidar <kushagraptdr18@gmail.com>
2025-04-08 13:48:26 +0530

    some

--- src/Components/utils/AxiosInstance.jsx
+++ src/Components/utils/AxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios with custom configurations
 const AxiosInstance = axios.create({
-  baseURL:"https://splitwisebackend.onrender.com", // Base URL for your API
+  baseURL:"https://splitwisebackend.onrender.com/", // Base URL for your API
   headers: {
     "Content-Type": "application/json", // Set default content type to JSON
   },
