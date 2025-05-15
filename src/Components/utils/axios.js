3a4e7bbfc2cf85521323fc2b8410e36762d780dc
Kushagra Patidar <kushagraptdr18@gmail.com>
2025-05-15 22:23:24 +0530

    changes in axios.js

--- src/Components/utils/axios.js
+++ src/Components/utils/axios.js
@@ -2,12 +2,25 @@ import axios from "axios";
 
 // Create an instance of axios with custom configurations
 const AxiosInstance = axios.create({
-  baseURL:"https://splitwisebackend.onrender.com", // Base URL for your API
+  baseURL: "https://splitwisebackend.onrender.com",
   headers: {
-    "Content-Type": "application/json", // Set default content type to JSON
+    "Content-Type": "application/json",
   },
-  withCredentials: true, // Include cookies for cross-origin requests (if you're using cookies)
+  withCredentials: true,
 });
 
+// Add a request interceptor to attach token
+AxiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // Get token from localStorage
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`; // Attach token to Authorization header
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default AxiosInstance;
